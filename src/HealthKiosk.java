fa32e649cd50d90fcf8e73cae4131bc23f4bee2b
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2025-09-28 00:14:19 +0000

    I have started.

---
+++ src/HealthKiosk.java
@@ -0,0 +1,16 @@
+//Date; 29/09/2025
+//Author: Harriet Yayra Boven Fiahagbe
+//Description:This is a Health Kiosk Intake System for Ashesi University's Health Center. The program is a self-service kiosk where students can select a health service, calculate their preferred health metrics, generate and validate their student ID, create a secure display code, and print a summary slip.
+
+
+//Importing Scanner package to allow input
+import java.util.Scanner;
+public class HealthKiosk {
+    public static void main(String[] args) {
+        //Creating Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+
+    }
+
+}
